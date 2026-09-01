363568a9c68976eb0a097eeadf51bfb271161be1
Aladin91 <alejandrofranco2407@gmail.com>
2026-09-01 15:50:45 -0400

    Expose vision and GitHub cloud modules

--- aladin-ai/src/index.js
+++ aladin-ai/src/index.js
@@ -5,6 +5,8 @@ export * from './neural.js';
 export * from './browser.js';
 export * from './retrieval.js';
 export * from './regression-trees.js';
+export * from './vision.js';
+export * from './github-cloud.js';
 
 import Base from './aladin-ai.js';
 import {
@@ -22,8 +24,14 @@ import { NeuralNetworkClassifier, NeuralNetworkRegressor } from './neural.js';
 import { schemaFingerprint, ModelPackage, IndexedDBModelStore, processInBatches, getRuntimeCapabilities } from './browser.js';
 import { CountVectorizer, FeatureHasher, BM25Index, TextSimilarityIndex, NearestNeighborsIndex, reciprocalRankFusion } from './retrieval.js';
 import { DecisionTreeRegressor, RandomForestRegressor } from './regression-trees.js';
+import {
+  validateImage,toGrayscale,imageFromCanvas,imageFromSource,resizeNearest,histogram,convolve,Kernels,sobelEdges,
+  otsuThreshold,threshold,connectedComponents,averageHash,differenceHash,hammingDistance,perceptualSimilarity,
+  templateMatch,colorKMeans,imageDescriptor,compareDescriptors
+} from './vision.js';
+import { GitHubCloud, GitHubReleaseAssetResolver, LocalFirstRepository, assertNoEmbeddedGitHubToken, GitHubCloudPatterns } from './github-cloud.js';
 
-export const VERSION = '0.3.0';
+export const VERSION = '0.4.0';
 
 export function createFullRegistry(extra={}){
   return createDefaultRegistry({
@@ -48,7 +56,11 @@ export const AladinAI = {
   NeuralNetworkClassifier, NeuralNetworkRegressor,
   schemaFingerprint, ModelPackage, IndexedDBModelStore, processInBatches, getRuntimeCapabilities,
   CountVectorizer, FeatureHasher, BM25Index, TextSimilarityIndex, NearestNeighborsIndex, reciprocalRankFusion,
-  DecisionTreeRegressor, RandomForestRegressor
+  DecisionTreeRegressor, RandomForestRegressor,
+  validateImage,toGrayscale,imageFromCanvas,imageFromSource,resizeNearest,histogram,convolve,Kernels,sobelEdges,
+  otsuThreshold,threshold,connectedComponents,averageHash,differenceHash,hammingDistance,perceptualSimilarity,
+  templateMatch,colorKMeans,imageDescriptor,compareDescriptors,
+  GitHubCloud,GitHubReleaseAssetResolver,LocalFirstRepository,assertNoEmbeddedGitHubToken,GitHubCloudPatterns
 };
 
 export default AladinAI;
